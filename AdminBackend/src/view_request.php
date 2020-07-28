5acd9ffc9276f7fb4dad3f5d1e4ad42390ed5199
sayuriii29 <sayuribogahawatta@gmail.com>
2020-07-28 19:11:24 +0530

    no message

--- AdminBackend/src/view_request.php
+++ AdminBackend/src/view_request.php
@@ -107,10 +107,10 @@
                             <div class="sidebar-submenu">
                                 <ul>
                                     <li>
-                                        <a href="#">Add Branches</a>
+                                        <a href="add_branch.php">Add Branches</a>
                                     </li>
                                     <li>
-                                        <a href="#">View Branches</a>
+                                        <a href="view_branch.php">View Branches</a>
                                     </li>
                                     
                                 </ul>
@@ -134,7 +134,7 @@
                                         <a href="add_employee.php">Add Employee</a>
                                     </li>
                                     <li>
-                                        <a href="#">View Employee</a>
+                                        <a href="view_employee.php">View Employee</a>
                                     </li>
                                    
                                 </ul>
